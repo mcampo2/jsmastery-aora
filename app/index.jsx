3443e2fda5f53271928e8f6e4eb6e919a3e86d9f
Michael Campo <mcampo2@gmail.com>
2024-05-07 21:07:57 -0400

    Created welcome message

---
+++ app/index.jsx
@@ -0,0 +1,28 @@
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { useFonts } from 'expo-font';
+import { Stack } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
+
+import { useColorScheme } from '@/hooks/useColorScheme';
+import { StyleSheet, Text, View } from 'react-native';
+
+function RootLayout() {
+
+  return (
+    <View style={styles.container}>
+      <Text>Welcome to Aora!</Text>
+    </View>
+  );
+}
+
+export default RootLayout;
+
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+})
