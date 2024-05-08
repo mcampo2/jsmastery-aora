e9fd5125452349532272418c11a41b08f929f15b
Michael Campo <mcampo2@gmail.com>
2024-05-07 21:09:11 -0400

    Created welcome message

--- app/index.jsx
+++ app/index.jsx
@@ -1,10 +1,3 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-
-import { useColorScheme } from '@/hooks/useColorScheme';
 import { StyleSheet, Text, View } from 'react-native';
 
 function RootLayout() {
